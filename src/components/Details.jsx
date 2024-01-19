eca3be4241164d6a50257129afe64d328882f832
Mbonu Basil <mbonubasil@gmail.com>
2024-01-19 01:52:34 -0500

    Use Google fonts and reduce text

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,10 +2,10 @@ export default function Details() {
     return (
         <main className="details">
             <h3>About</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus deserunt reprehenderit magnam maxime dolores, laudantium eaque tenetur totam iste mollitia.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus deserunt reprehenderit magnam maxime dolores, laudantium eaque.</p>
 
             <h3>Interests</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus deserunt reprehenderit magnam maxime dolores, laudantium eaque tenetur totam iste mollitia.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus deserunt reprehenderit magnam maxime dolores, laudantium eaque.</p>
         </main>
     )
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,9 +4,9 @@ export default function Title() {
     return (
         <div className='title'>
             <img src={contactImage} alt="contact image" className='contact-image' />
-            <h1>Laura Smith</h1>
-            <h4>Frontend Developer</h4>
-            <h6>laurasmith.website</h6>
+            <h1 className='name'>Laura Smith</h1>
+            <h4 className='skill'>Frontend Developer</h4>
+            <h6 className='website'>laurasmith.website</h6>
             <div className='title-btn'>
                 <button className='email-btn'>Email</button>
                 <button className='linkedin-btn'>LinkedIn</button>
