49630de630aed8ea773ac983dbe8e6da9be57085
Mbonu Basil <mbonubasil@gmail.com>
2024-01-19 01:25:47 -0500

    Create other components that represent the page

---
+++ src/components/Details.jsx
@@ -0,0 +1,11 @@
+export default function Details() {
+    return (
+        <main className="details">
+            <h3>About</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus deserunt reprehenderit magnam maxime dolores, laudantium eaque tenetur totam iste mollitia.</p>
+
+            <h3>Interests</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus deserunt reprehenderit magnam maxime dolores, laudantium eaque tenetur totam iste mollitia.</p>
+        </main>
+    )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import Social from "./Social";
+
+export default function Footer() {
+    return (
+        <footer>
+            <Social />
+            <Social />
+            <Social />
+            <Social />
+        </footer>
+    )
+}
